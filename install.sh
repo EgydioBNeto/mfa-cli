fbc1120ada6090f9ec9bd5f5181117ce8074510e
Egydio Bolonhezi <egydiobolonhezi@gmail.com>
2023-12-06 14:50:24 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,23 +20,24 @@ alias mfa_export='~/mfa-cli/script.py export_secrets --export_file secrets.json'
 function mfa_add() {
     ~/mfa-cli/script.py add_secret --name "$1" --secret "$2"
 }
-alias 'mfa_add'='mfa_add'
+alias mfa_add='mfa_add()'
 function mfa_delete() {
     ~/mfa-cli/script.py delete_secret --name "$1"
 }
-alias 'mfa_delete'='mfa_delete'
+alias mfa_delete='mfa_delete()'
 function mfa_list() {
     ~/mfa-cli/script.py list_secrets
 }
-alias 'mfa_list'='mfa_list'
+alias mfa_list='mfa_list()'
 function mfa_generate() {
     ~/mfa-cli/script.py generate_mfa --name "$1"
 }
-alias 'mfa_generate'='mfa_generate'
+alias mfa_generate='mfa_generate()'
 function mfa_update() {
     ~/mfa-cli/script.py update_secret --name "$1" --secret "$2"
 }
-alias 'mfa_update'='mfa_update'
+alias mfa_update='mfa_update()'
+alias mfa_help='~/mfa-cli/script.py help'
 " >> "$config_file"
     
 done
