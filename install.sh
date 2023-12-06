2a2b182d01dbdeb0ff98c45fae223bf1f97ca781
Egydio Bolonhezi <egydiobolonhezi@gmail.com>
2023-12-06 14:04:42 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,34 +15,29 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for config_file in "${CONFIG_FILES[@]}"; do
 
-    echo "
-        alias mfa_export='/home/egydio/mfa-cli/script.py export_secrets --export_file secrets.json'
-
-        function mfa add() {
-            /home/egydio/mfa-cli/script.py add_secret --name "$1" --secret "$2"
-        }
-        alias 'mfa_add'='mfa_add'
-
-        function mfa delete() {
-            /home/egydio/mfa-cli/script.py delete_secret --name "$1"
-        }
-        alias 'mfa_delete'='mfa_delete'
-
-        function mfa list() {
-            /home/egydio/mfa-cli/script.py list_secrets
-        }
-        alias 'mfa_list'='mfa_list'
-
-        function mfa generate() {
-            /home/egydio/mfa-cli/script.py generate_mfa --name "$1"
-        }
-        alias 'mfa_generate'='mfa_generate'
-
-        function mfa update() {
-            /home/egydio/mfa-cli/script.py update_secret --name "$1" --secret "$2"
-        }
-        alias 'mfa_update'='mfa_update'
-            " >> "$config_file"
+echo "
+alias mfa_export='/home/egydio/mfa-cli/script.py export_secrets --export_file secrets.json'
+function mfa add() {
+    /home/egydio/mfa-cli/script.py add_secret --name "$1" --secret "$2"
+}
+alias 'mfa_add'='mfa_add'
+function mfa delete() {
+    /home/egydio/mfa-cli/script.py delete_secret --name "$1"
+}
+alias 'mfa_delete'='mfa_delete'
+function mfa list() {
+    /home/egydio/mfa-cli/script.py list_secrets
+}
+alias 'mfa_list'='mfa_list'
+function mfa generate() {
+    /home/egydio/mfa-cli/script.py generate_mfa --name "$1"
+}
+alias 'mfa_generate'='mfa_generate'
+function mfa update() {
+    /home/egydio/mfa-cli/script.py update_secret --name "$1" --secret "$2"
+}
+alias 'mfa_update'='mfa_update'
+" >> "$config_file"
     
 done
 
