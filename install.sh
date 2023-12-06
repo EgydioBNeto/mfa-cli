28d38ee81a7ba849a8c8fa75b9464ad1703740d3
Egydio Bolonhezi <egydiobolonhezi@gmail.com>
2023-12-06 17:43:54 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,15 +15,26 @@ for config_file in "${CONFIG_FILES[@]}"; do
 
 echo "
 # MFA CLI aliases start
-alias 'mfa export'='$INSTALL_DIR/$SCRIPT_NAME export_secrets --export_file secrets.json'
-alias 'mfa add'='$INSTALL_DIR/$SCRIPT_NAME add_secret'
-alias 'mfa list'='$INSTALL_DIR/$SCRIPT_NAME list_secrets'
-alias 'mfa generate'='$INSTALL_DIR/$SCRIPT_NAME generate_mfa'
-alias 'mfa update'='$INSTALL_DIR/$SCRIPT_NAME update_secret'
-alias 'mfa delete'='$INSTALL_DIR/$SCRIPT_NAME delete_secret'
-alias 'mfa help'='$INSTALL_DIR/$SCRIPT_NAME help'
+alias mfa_export='$INSTALL_DIR/$SCRIPT_NAME export_secrets --export_file secrets.json'
+alias mfa_add='$INSTALL_DIR/$SCRIPT_NAME add_secret'
+alias mfa_list='$INSTALL_DIR/$SCRIPT_NAME list_secrets'
+alias mfa_generate='$INSTALL_DIR/$SCRIPT_NAME generate_mfa'
+alias mfa_update='$INSTALL_DIR/$SCRIPT_NAME update_secret'
+alias mfa_delete='$INSTALL_DIR/$SCRIPT_NAME delete_secret'
+alias mfa_help='$INSTALL_DIR/$SCRIPT_NAME help'
+alias mfe='$INSTALL_DIR/$SCRIPT_NAME export_secrets --export_file secrets.json'
+alias mfa='$INSTALL_DIR/$SCRIPT_NAME add_secret'
+alias mfl='$INSTALL_DIR/$SCRIPT_NAME list_secrets'
+alias mfg='$INSTALL_DIR/$SCRIPT_NAME generate_mfa'
+alias mfu='$INSTALL_DIR/$SCRIPT_NAME update_secret'
+alias mfd='$INSTALL_DIR/$SCRIPT_NAME delete_secret'
+alias mfh='$INSTALL_DIR/$SCRIPT_NAME help'
 # MFA CLI aliases end
 " >> "$config_file"
+
+    
 done
+
+
 
 echo "Script installed successfully, please restart your terminal!"
