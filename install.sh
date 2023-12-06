451cc886e21b1eb202b66d6e424c6534672766b2
Egydio Bolonhezi <egydiobolonhezi@gmail.com>
2023-12-06 17:29:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,13 +14,15 @@ chmod +x "$INSTALL_DIR/$SCRIPT_NAME"
 for config_file in "${CONFIG_FILES[@]}"; do
 
 echo "
-alias mfa_export='$INSTALL_DIR/$SCRIPT_NAME export_secrets --export_file secrets.json'
-alias mfa_add='$INSTALL_DIR/$SCRIPT_NAME add_secret'
-alias mfa_list='$INSTALL_DIR/$SCRIPT_NAME list_secrets'
-alias mfa_generate='$INSTALL_DIR/$SCRIPT_NAME generate_mfa'
-alias mfa_update='$INSTALL_DIR/$SCRIPT_NAME update_secret'
-alias mfa_delete='$INSTALL_DIR/$SCRIPT_NAME delete_secret'
-alias mfa_help='$INSTALL_DIR/$SCRIPT_NAME help'
+# MFA CLI aliases start
+alias mfa export='$INSTALL_DIR/$SCRIPT_NAME export_secrets --export_file secrets.json'
+alias mfa add='$INSTALL_DIR/$SCRIPT_NAME add_secret'
+alias mfa list='$INSTALL_DIR/$SCRIPT_NAME list_secrets'
+alias mfa generate='$INSTALL_DIR/$SCRIPT_NAME generate_mfa'
+alias mfa update='$INSTALL_DIR/$SCRIPT_NAME update_secret'
+alias mfa delete='$INSTALL_DIR/$SCRIPT_NAME delete_secret'
+alias mfa help='$INSTALL_DIR/$SCRIPT_NAME help'
+# MFA CLI aliases end
 " >> "$config_file"
 
     
