9ab521ca78a38893c3887dfd158827ccc5b697bf
Egydio Bolonhezi <egydiobolonhezi@gmail.com>
2023-12-06 19:48:13 -0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -9,4 +9,4 @@ for config_file in "${CONFIG_FILES[@]}"; do
     sed -i '/# MFA CLI aliases start/,/# MFA CLI aliases end/d' "$config_file"
 done
 
-echo "Uninstallation completed successfully. Please restart your terminal to apply the changes."
+echo -e "\033[92m Environment clenup complete.\033[0m"
