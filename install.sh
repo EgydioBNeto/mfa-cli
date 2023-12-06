ae8992b3a2b46414f148b4e41531a840a3cf7c06
Egydio Bolonhezi <egydiobolonhezi@gmail.com>
2023-12-06 18:49:54 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,14 +15,14 @@ for config_file in "${CONFIG_FILES[@]}"; do
 
 echo "
 # MFA CLI aliases start
-alias mfa_export='$INSTALL_DIR/$SCRIPT_NAME export_secrets --export_file secrets.json'
+alias mfa_export='$INSTALL_DIR/$SCRIPT_NAME export_secrets --export_file'
 alias mfa_add='$INSTALL_DIR/$SCRIPT_NAME add_secret'
 alias mfa_list='$INSTALL_DIR/$SCRIPT_NAME list_secrets'
 alias mfa_generate='$INSTALL_DIR/$SCRIPT_NAME generate_mfa'
 alias mfa_update='$INSTALL_DIR/$SCRIPT_NAME update_secret'
 alias mfa_delete='$INSTALL_DIR/$SCRIPT_NAME delete_secret'
 alias mfa_help='$INSTALL_DIR/$SCRIPT_NAME help'
-alias mfe='$INSTALL_DIR/$SCRIPT_NAME export_secrets --export_file secrets.json'
+alias mfe='$INSTALL_DIR/$SCRIPT_NAME export_secrets --export_file'
 alias mfa='$INSTALL_DIR/$SCRIPT_NAME add_secret'
 alias mfl='$INSTALL_DIR/$SCRIPT_NAME list_secrets'
 alias mfg='$INSTALL_DIR/$SCRIPT_NAME generate_mfa'
@@ -32,9 +32,6 @@ alias mfh='$INSTALL_DIR/$SCRIPT_NAME help'
 # MFA CLI aliases end
 " >> "$config_file"
 
-    
 done
-
-
 
 echo "Script installed successfully, please restart your terminal!"
