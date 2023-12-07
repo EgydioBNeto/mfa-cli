52df2782e25d2bfbda642f3a1370cb732f2af35d
egydio-neto <egydio.neto@tmaior.com.br>
2023-12-07 15:26:11 -0300

    fix re-install error

--- install.py
+++ install.py
@@ -21,7 +21,8 @@ RESET = "\033[0m"
 
 if os.path.exists(INSTALL_DIR):
     print("'mfa-cli' already exists. Proceeding with cleanup.")
-    subprocess.run(["python3", "-c", f"['curl', '-fsSL', '{UNINSTALL_URL}'], check=True, shell=True)"], check=True)
+    subprocess.run(["python3", "-c", f"import subprocess; subprocess.run(['curl', '-fsSL', '{UNINSTALL_URL}'], check=True, shell=True)"])
+
 
 else:
     print("Directory 'mfa-cli' does not exist. Proceeding with installation.")
